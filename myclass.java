e90a5b840fb573c354dc1bcae0b312a36d1f9b6a
Md Umar Faruk <mdumarfaruk793@gmail.com>
2025-02-19 20:19:08 +0530

    Add files via upload

---
+++ myclass.java
@@ -0,0 +1,14 @@
+class myclass
+{
+int banch;
+String name;
+public static void main(String[] args)
+{
+myclass A1 = new myclass();
+A1.banch = 10;
+A1.name = "umar";
+System.out.println(A1.banch);
+System.out.println(A1.name);
+
+}
+}
